6e33d6335960db35539f16177bc301207b356451
ljgiron17 <85210908+ljgiron17@users.noreply.github.com>
2021-06-13 22:57:15 -0600

    Update Global.php

--- clases/global.php
+++ clases/global.php
@@ -1,15 +1,15 @@
 <?php
 //Ip de la pc servidor de base de datos
-define("DB_HOST","167.114.169.207");
+define("DB_HOST","localhost");
 
 //Nombre de la base de datos
-define("DB_NAME", "informat_automatizacion");
+define("DB_NAME", "informat_desarrollo_automatizacion");
 
 //Usuario de la base de datos
-define("DB_USERNAME", "informat_admin");
+define("DB_USERNAME", "root");
 
 //Contraseña del usuario de la base de datos
-define("DB_PASSWORD", "HLo{Q3e{)II^");
+define("DB_PASSWORD", " ");
 
 //definimos la codificación de los caracteres
 define("DB_ENCODE","utf8");
